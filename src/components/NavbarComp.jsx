8c44cfff413d20cec88d4c0f4ce31f12b8ce5811
BikramGyawali <gyawalibikram0@gmail.com>
2025-11-15 10:40:02 +0545

    Made navbar static

--- src/components/NavbarComp.jsx
+++ src/components/NavbarComp.jsx
@@ -1,59 +1,70 @@
-import React from 'react';
-import logo from '../assets/image/logo.png';
+import React from "react";
+import logo from "../assets/image/logo.png";
 import {
 	Navbar,
 	NavbarBrand,
 	NavbarCollapse,
 	NavbarLink,
 	NavbarToggle,
-} from 'flowbite-react';
+} from "flowbite-react";
 
 function NavbarComp() {
 	return (
-		<div>
-			<Navbar fluid rounded className="!bg-white shadow-md">
+		<div className="sticky top-0 z-50 shadow-md">
+			<Navbar fluid rounded className="!bg-white">
 				<NavbarBrand href="/">
-					<img src={logo} className="mr-3 p-0 h-10  sm:h-20 rounded-full" alt="Company Logo" />
-					<span className="self-center whitespace-nowrap text-xl font-semibold !text-black">
+					<img
+						src={logo}
+						className="mr-2 h-10 w-10 sm:h-12 sm:w-12 rounded-full object-cover"
+						alt="Company Logo"
+					/>
+					<span className="self-center whitespace-nowrap text-2xl font-bold text-gray-800">
 						MyCompany
 					</span>
 				</NavbarBrand>
+
 				<NavbarToggle />
-				<NavbarCollapse className=''>
+
+				<NavbarCollapse>
 					<NavbarLink
 						href="#"
 						active
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300  "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						Home
 					</NavbarLink>
+
 					<NavbarLink
 						href="#"
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300 "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						About
 					</NavbarLink>
+
 					<NavbarLink
 						href="#"
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300 "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						Services
 					</NavbarLink>
+
 					<NavbarLink
 						href="#"
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300 "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						Contact
 					</NavbarLink>
+
 					<NavbarLink
 						href="#"
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300 "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						JobSeeker
 					</NavbarLink>
+
 					<NavbarLink
 						href="#"
-						className="!text-black text-[20px] hover:!text-blue-500 transition-colors duration-300 "
+						className="text-lg !text-gray-800 hover:!text-blue-600 transition duration-300"
 					>
 						Employers
 					</NavbarLink>

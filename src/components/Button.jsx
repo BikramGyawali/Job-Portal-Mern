a7271b08fc64555b3bdf4fac64edfbc7e02315df
BikramGyawali <gyawalibikram0@gmail.com>
2025-11-16 14:57:20 +0545

    Added button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Button() {
+	return (
+		<button
+			type="submit"
+			className="bg-blue-600 text-white py-2 rounded-xl cursor-pointer hover:bg-blue-700 transition w-full"
+		>
+			Submit
+		</button>
+	)
+}
+
+export default Button
--- src/components/LoginComp.jsx
+++ src/components/LoginComp.jsx
@@ -9,8 +9,27 @@ import RegisterComp from './RegisterComp';
 
 function LoginComp({ LoginData }) {
 	const [showPassword, setShowPassword] = useState(false)
-	// console.log(loginData);
+	const [form, setForm] = useState({
+		email: "",
+		pass: ""
+	})
 
+	const handleChange = (e) => {
+		setForm({
+
+			...form,
+			[e.target.name]: [e.target.value]
+		})
+	}
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(form);
+		setForm({
+			email: "",
+			pass: ""
+		})
+	}
 
 	const { title, explain, image } = LoginData;
 
@@ -25,32 +44,40 @@ function LoginComp({ LoginData }) {
 			<div className="flex flex-col items-center bg-white/10 md:!bg-gradient-to-r from-blue-100 to-blue-200 backdrop-blur-md p-6 rounded-2xl shadow-lg">
 				<p className='text-2xl text-center font-bold text-black tracking-tight'>{title}</p>
 				<p className='text-[20px] text-center  text-black mt-2'>{explain}</p>
-				<div className="w-full flex flex-col mb-3">
-					<label className="text-2xl font-semibold mb-2 ">Email</label>
-					<input
-						type="text"
-						placeholder="Your Email"
-						className="w-full text-xl p-3 rounded-2xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 mb-4"
-					/>
-
-				</div>
-				<div className="w-full flex flex-col mb-3">
-					<label className="text-2xl font-semibold mb-2">Password</label>
-					<div className="relative w-full">
+				<form action="" className="w-full max-w-md" onSubmit={handleSubmit}>
+					<div className="w-full flex flex-col mb-1">
+						<label className="text-2xl font-semibold mb-2 ">Email</label>
 						<input
-							type={showPassword ? "text" : "password"}
-							placeholder="Your Password"
-							className="w-full text-xl p-3 rounded-2xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+							type="text"
+							placeholder="Your Email"
+							onChange={handleChange}
+							value={form.email}
+							name='email'
+							className="w-full text-xl p-3 rounded-2xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400 mb-4"
 						/>
-						<button
-							type="button"
-							onClick={() => setShowPassword(!showPassword)}
-							className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-900"
-						>
-							<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
-						</button>
+
 					</div>
-				</div>
+					<div className="w-full flex flex-col mb-3">
+						<label className="text-2xl font-semibold mb-2">Password</label>
+						<div className="relative w-full">
+							<input
+								type={showPassword ? "text" : "password"}
+								placeholder="Your Password"
+								onChange={handleChange}
+								value={form.pass}
+								name='pass'
+								className="w-full text-xl p-3 rounded-2xl border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
+							/>
+							<button
+								type="button"
+								onClick={() => setShowPassword(!showPassword)}
+								className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-600 hover:text-gray-900"
+							>
+								<FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+							</button>
+						</div>
+					</div>
+				</form>
 				<p className='text-2xl p-3 m-3 '>New to Hamro Job  {' '}
 					<Link to="/register" className='text-blue-900 underline'
 						onClick={RegisterComp}>

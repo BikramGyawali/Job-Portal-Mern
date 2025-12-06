bd490ef0f587d2246908d77f8747e8ac45aaf6c9
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-06 23:29:55 +0545

    Added responsive cards

--- src/components/common/DasCard.jsx
+++ src/components/common/DasCard.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 
 function DasCard({ data }) {
-	const { number, content } = data
+	const { number, content } = data;
+
 	return (
-		<div className="min-w-[320px] min-h-5 p-3 m-2 rounded-2xl overflow-hidden bg-linear-to-r from-indigo-500 to-purple-500 text-white cursor-pointer transition-all duration-700 hover:scale-105">
-			<h1 className="text-2xl font-bold">{number}</h1>
-			<h2 className="text-2xl font-semibold">{content}</h2>
+		<div
+			className=" w-full p-5 sm:p-6  rounded-2xl bg-linear-to-r from-indigo-500 to-purple-500  text-white cursor-pointer  shadow-md hover:shadow-xl   transition-transform duration-500   hover:scale-105
+      "
+		>
+			<h1 className="text-xl sm:text-2xl font-bold">{number}</h1>
+			<h2 className="text-lg sm:text-xl font-semibold mt-1">{content}</h2>
 		</div>
 	);
 }
--- src/components/common/DashBoxCard.jsx
+++ src/components/common/DashBoxCard.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import { DashboardCardData } from '../../data/employers/DashboardCardData'
-import DasCard from './DasCard'
+import React from 'react';
+import { DashboardCardData } from '../../data/employers/DashboardCardData';
+import DasCard from './DasCard';
 
 function DashBoxCard() {
 	return (
-		<div className='shadow-lg backdrop-blur-4xl bg-white grid grid-cols-2 mx-10 p-3 rounded-2xl w-[700px] min-h-5'>
-			{DashboardCardData.map((data, i) =>
-			(
+		<div className="
+      grid  grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  gap-5 shadow-lg   bg-white   backdrop-blur-4xl    mx-5 md:mx-10 p-4 md:p-6 rounded-2xl w-full ">
+			{DashboardCardData.map((data, i) => (
 				<DasCard data={data} key={i} />
-			)
-			)}
+			))}
 		</div>
-	)
+	);
 }
 
-export default DashBoxCard
+export default DashBoxCard;

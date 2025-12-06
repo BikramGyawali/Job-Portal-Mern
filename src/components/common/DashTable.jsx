a51200871514fbc7c6ae012357fc90bd6659e151
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-07 00:18:52 +0545

    added table for dashboard

--- src/components/common/DashTable.jsx
+++ src/components/common/DashTable.jsx
@@ -1,56 +1,62 @@
 import React from "react";
 import {
-  Table,
-  TableHead,
-  TableHeadCell,
-  TableBody,
-  TableRow,
-  TableCell,
+	Table,
+	TableHead,
+	TableHeadCell,
+	TableBody,
+	TableRow,
+	TableCell,
 } from "flowbite-react";
 
 function DashTable({ title, headData, bodyData }) {
-  return (
-    <div className="bg-white p-4 rounded-2xl shadow-md w-full">
-      {/* Header */}
-      <div className="flex justify-between mb-4">
-        <p className="text-[20px] font-semibold">{title}</p>
-        <p className="text-[20px] font-semibold cursor-pointer hover:underline">
-          View All
-        </p>
-      </div>
+	return (
+		<div className=" p-4 rounded-2xl shadow-md w-full">
 
-      {/* Table */}
-      <Table>
-        {/* ======= Table Head ======= */}
-        <TableHead>
-          <TableRow>
-            {headData.map((head, i) => (
-              <TableHeadCell key={i}>{head}</TableHeadCell>
-            ))}
-          </TableRow>
-        </TableHead>
+			<div className="flex justify-between mb-4">
+				<p className="text-[20px] font-semibold">{title}</p>
+				<p className="text-[20px] font-semibold cursor-pointer hover:underline">
+					View All
+				</p>
+			</div>
 
-        {/* ======= Table Body ======= */}
-        <TableBody>
-          {bodyData.length === 0 ? (
-            <TableRow>
-              <TableCell colSpan={headData.length} className="text-center py-4">
-                No data available
-              </TableCell>
-            </TableRow>
-          ) : (
-            bodyData.map((row, i) => (
-              <TableRow key={i}>
-                {headData.map((key, j) => (
-                  <TableCell key={j}>{row[key]}</TableCell>
-                ))}
-              </TableRow>
-            ))
-          )}
-        </TableBody>
-      </Table>
-    </div>
-  );
+
+			<Table >
+				<TableHead >
+					<TableRow >
+						{headData.map((head, i) => (
+							<TableHeadCell key={i} className="!text-black !font-bold !bg-gray-100">
+								{head}
+							</TableHeadCell>
+						))}
+					</TableRow>
+				</TableHead>
+
+				<TableBody>
+					{bodyData.length === 0 ? (
+						<TableRow className="!bg-white">
+							<TableCell
+								colSpan={headData.length}
+								className="!text-black text-center py-4"
+							>
+								No data available
+							</TableCell>
+						</TableRow>
+					) : (
+						bodyData.map((row, i) => (
+							<TableRow key={i} className=" hover:!bg-gray-50">
+								{headData.map((key, j) => (
+									<TableCell key={j} className="!text-black">
+										{row[key]}
+									</TableCell>
+								))}
+							</TableRow>
+						))
+					)}
+				</TableBody>
+			</Table>
+
+		</div>
+	);
 }
 
 export default DashTable;

80a0045c5dbe1dac9d18ae0afa87a77ecd1a7d9d
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-07 23:37:41 +0545

    added event handlers in my jobs

--- src/components/dashboard/Employer/pages/MyJobs.jsx
+++ src/components/dashboard/Employer/pages/MyJobs.jsx
@@ -3,9 +3,20 @@ import DashTable from '../../../common/DashTable'
 import { MyJobsTableBody, MyJobsTableHead } from '../../../../data/employers/DashboardData'
 
 function MyJobs() {
+	const handleEdit = (row) => {
+		console.log("Edit", row);
+	}
+	const handleDelete = (row) => {
+		console.log("Delete", row);
+
+	}
+	const actionHandler = {
+		Edit: handleEdit,
+		Delete: handleDelete
+	}
 	return (
 		<div>
-			<DashTable headData={MyJobsTableHead} bodyData={MyJobsTableBody} title="My Jobs" />
+			<DashTable headData={MyJobsTableHead} bodyData={MyJobsTableBody} title="My Jobs" actionHandler={actionHandler} />
 		</div>
 	)
 }

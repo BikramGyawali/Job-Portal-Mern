a836677130d781d403a71b520341998f13f14596
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-18 00:16:22 +0545

    create a database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import express, { json } from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./src/config/database.js";
+import { jobseekerSignupModel } from "./src/models/jobseeker/jobseekerSignup.model.js";
 
 dotenv.config();
 const app = express();
@@ -8,6 +9,20 @@ app.use(express.json());
 const PORT = process.env.PORT || 5000;
 
 connectDB();
+app.get("/test", async (req, res) => {
+	try {
+		const user = new jobseekerSignupModel({
+			email: "test@example.com",
+			pass: "123456",
+			cpass: "123456"
+		});
+		await user.save();
+		res.send("User created and DB should now exist!");
+	} catch (err) {
+		res.status(500).send(err.message);
+	}
+});
+
 app.listen(PORT, () => {
 	console.log(`Serving running at port number ${PORT}`);
 

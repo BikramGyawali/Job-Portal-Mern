fb75968e34fe6ad6eb92d8e485068bc708fa5d4d
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-25 22:19:20 +0545

    reissue token

--- backend/src/controllers/Profile/ProfileController.js
+++ backend/src/controllers/Profile/ProfileController.js
@@ -1,13 +1,16 @@
 
 import express from "express"
 import fs from "fs"
+import cookieParser from "cookie-parser";
 import path from "path"
+import jwt from "jsonwebtoken"
+import dotenv from dotenv
 import { JobseekerProfile } from "../../models/jobseeker/JobseekerProfile.js"
 import { EmployerProfile } from "../../models/employer/EmployerProfile.js"
 import { User } from "../../models/LoginModel/SignupLogic.js"
 const app = express()
 app.use(express.json())
-
+dotenv.config();
 // Helper to write image buffer to disk and return filename
 const saveImageBuffer = async (file) => {
 	if (!file || !file.buffer) return null;
@@ -20,6 +23,18 @@ const saveImageBuffer = async (file) => {
 	const filePath = path.join(uploadDir, fileName)
 	await fs.promises.writeFile(filePath, file.buffer)
 	return fileName
+}
+
+//function to create new token generator
+const generateToken = (user) => {
+	return jwt.sign(
+		{
+			id: user._id,
+			role: user.role,
+			isProfileCompleted: user.isProfileCompleted
+		}, process.env.JWT_KEY, { expiresIn: "1d" }
+
+	)
 }
 
 // Jobseeker Profile
@@ -53,13 +68,23 @@ export const JProfileController = async (req, res) => {
 			}
 		}
 
-		await User.findByIdAndUpdate(userId, {
+		const user = await User.findByIdAndUpdate(userId, {
 			isProfileCompleted: true
-		});
+		},
+			{ new: true }
+		);
+		const token = generateToken(user)
+		res.cookie("token", token, {
+			httpOnly: true,
+			samesite: lax,
+			secure: false
+		}
 
+		)
 		res.status(201).json({
 			status: 1,
 			message: "Profile created",
+			isProfileCompleted: true,
 			profile
 		});
 	} catch (err) {
@@ -95,13 +120,24 @@ export const EProfileController = async (req, res) => {
 			}
 		}
 
-		await User.findByIdAndUpdate(userId, {
+		const user = await User.findByIdAndUpdate(userId, {
 			isProfileCompleted: true
-		});
+		},
+			{ new: true },
+		);
 
+		const token = generateToken(user)
+		res.cookie("token", token, {
+			httpOnly: true,
+			samesite: lax,
+			secure: false
+		}
+
+		)
 		res.status(201).json({
 			status: 1,
 			message: "Profile created",
+			isProfileCompleted: true,
 			profile
 		});
 	} catch (err) {

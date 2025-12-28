9de970a3e9b09d531f614935a6f6980d18221dfb
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-28 21:23:52 +0545

    changes ui of card

--- frontend/src/components/common/CardComp.jsx
+++ frontend/src/components/common/CardComp.jsx
@@ -5,7 +5,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faHourglassEnd, faLocation, faLocationDot } from "@fortawesome/free-solid-svg-icons";
 export function CardComp() {
 	return (
-		<Card href="#" className="max-w-sm !bg-white transition-transform duration-300 hover:scale-105 cursor-pointer">
+		<Card className="max-w-md !bg-white transition-transform duration-300 hover:scale-105 ">
 			<h5 className="text-2xl font-bold tracking-wide text-gray-900">
 				MERN Stack Developer
 			</h5>
@@ -17,9 +17,10 @@ export function CardComp() {
 				<FontAwesomeIcon icon={faHourglassEnd} className="text-red-500" title="Deadline Passed" />
 				<span>5 days</span>
 			</div>
-			<div className="flex flex-wrap gap-5">
+			<div className="flex flex-wrap gap-2">
 				<p className="text-lg bg-blue-400 p-2 text-white rounded-xl hover:bg-blue-500">Full Time</p>
 				<p className="text-lg bg-blue-400 p-2 text-white rounded-xl hover:bg-blue-500">Senior </p>
+				<button className="text-lg bg-blue-400 p-2 text-white rounded-xl hover:bg-blue-500 hover:cursor-pointer">Apply</button>
 			</div>
 
 		</Card>

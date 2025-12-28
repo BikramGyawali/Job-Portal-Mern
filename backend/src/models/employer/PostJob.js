631a82c4028801cf33b7c84602d49a09fbe4066d
BikramGyawali <gyawalibikram0@gmail.com>
2025-12-28 21:54:45 +0545

    added schema for the job

---
+++ backend/src/models/employer/PostJob.js
@@ -0,0 +1,69 @@
+import mongoose from "mongoose";
+const schema = mongoose.Schema;
+const postJob = new schema({
+	userId: {
+		type: mongoose.Schema.Types.ObjectId,
+		ref: "User",
+		required: true
+
+	},
+	jobTitle: {
+		type: String,
+		required: true
+	},
+	openings: Number,
+	mainCategory: {
+		type: String,
+		required: true
+	},
+	subCategory: String,
+	postingDate: {
+		type: Date,
+		required: true
+	},
+	postingPeriod: {
+		type: String,
+		required: true
+	},
+	jobLevel: {
+		type: String,
+		required: true
+	},
+	desiredCandidate: String,
+	educationLevel: String,
+	experience: String,
+	district: String,
+	municipality: String,
+	location: String,
+	salaryCurrency: String,
+	salaryPeriod: String,
+	salaryRange: String,
+	license: String,
+	vehicle: Boolean,
+	skills: {
+		type: [String],
+		default: []
+	},
+	jobDescription: {
+		type: String,
+		required: true
+	},
+	jobSpecification: {
+		type: String,
+		required: true
+	},
+	isApproved: {
+		type: Boolean,
+		default: true
+	},
+	// approvalDate: Date,
+	createdAt: {
+		type: Date,
+		default: Date.now
+	},
+	updatedAt: {
+		type: Date,
+		default: Date.now
+	}
+}, { timestamps: true })
+export const PostJob = mongoose.model("PostJob", postJob)

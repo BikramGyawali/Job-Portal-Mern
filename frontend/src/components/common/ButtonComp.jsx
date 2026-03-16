ee130acbfe61febcb87654632f947cb0329f86a0
BikramGyawali <gyawalibikram0@gmail.com>
2026-03-16 12:39:04 +0545

    Added style in the button

--- frontend/src/components/common/ButtonComp.jsx
+++ frontend/src/components/common/ButtonComp.jsx
@@ -1,22 +1,22 @@
 import React from 'react'
 
 function ButtonComp({ name, click, disable }) {
-<<<<<<< HEAD
-    return (
-        <button
-            type="submit"
-            className={`bg-blue-600 text-white py-2 px-3 rounded-xl hover:bg-blue-700 transition w-full 
-                ${disable 
-                    ? 'opacity-50 cursor-not-allowed' 
-                    : 'cursor-pointer'
-                }`}
-            onClick={click}
-            disabled={disable}
-        >
-            {name}
-        </button>
-    )
-	
+
+	return (
+		<button
+			type="submit"
+			className={`bg-blue-600 text-white py-2 px-3 rounded-xl hover:bg-blue-700 transition w-full 
+                ${disable
+					? 'opacity-50 cursor-not-allowed'
+					: 'cursor-pointer'
+				}`}
+			onClick={click}
+			disabled={disable}
+		>
+			{name}
+		</button>
+	)
+
 }
 
 export default ButtonComp

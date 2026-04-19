ded3131eb28b81a237cb0d4e48a0d4d6345a7a50
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-19 21:33:40 +0545

    Added backend url

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3000",
+	baseURL: "http://localhost:3000" || "https://hamrojob-backend.onrender.com",
 	withCredentials: true,  // sends cookies with every request
 	headers: {
 		"Content-Type": "application/json"

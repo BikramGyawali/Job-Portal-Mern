e533c8a86164e7be9453fb376d1389b9b64627ec
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-19 21:58:23 +0545

    fix: gif image path

--- frontend/src/pages/public/PageNotFound.jsx
+++ frontend/src/pages/public/PageNotFound.jsx
@@ -6,8 +6,8 @@ const PageNotFound = () => {
 
 
 		<div className="flex justify-center items-center flex-col gap-6 min-h-screen">
-			
-			<img src={pageimage} alt="Page Not Found" className=" items-center" />
+
+			<img src="../../assets/image/page404.gif" alt="Page Not Found" className=" items-center" />
 
 			<h1 className="text-2xl">Route to Home Page</h1>
 			<Link to={'/'} className="border-green-300 bg-green-400 p-2 rounded-2xl  hover:scale-115 duration-150  " replace={true}>Home</Link >

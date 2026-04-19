d28040f7a97393d3f39302314b09435a85d1573c
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-19 22:59:08 +0545

    fix: replace localhost with env variable for API URL

--- frontend/src/utils/profileapi.js
+++ frontend/src/utils/profileapi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = "https://hamrojob-backend.onrender.com"
 
 
 export const Profile = async (formData, role) => {

a8d07365f94e952f1a5f7716dae9b144d74fa327
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-22 20:46:46 +0545

    fix: ensure _redirects copies to dist

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,26 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { resolve } from "path";
+import fs from "fs";
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		// manually copy _redirects to dist
+		{
+			name: "copy-redirects",
+			closeBundle() {
+				fs.copyFileSync(
+					resolve(__dirname, "public/_redirects"),
+					resolve(__dirname, "dist/_redirects")
+				);
+			},
+		},
+	],
 	build: {
 		outDir: "dist",
 	},
-	publicDir: "public", 
+	publicDir: "public",
 });

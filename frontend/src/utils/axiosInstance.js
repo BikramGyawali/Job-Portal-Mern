24a1984fd035bd047edd4cf8b0bb8aacc5a52f24
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-22 22:10:42 +0545

    complete project hehe

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@
 import axios from "axios";
 
 const api = axios.create({
-	baseURL: "http://localhost:3000" || "https://hamrojob-backend.onrender.com",
+	baseURL: "http://localhost:3000",
 	withCredentials: true,  // sends cookies with every request
 	headers: {
 		"Content-Type": "application/json"

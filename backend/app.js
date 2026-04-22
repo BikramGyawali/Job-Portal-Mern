48d5f3d847573c54682ec4529241cdc680b0f5ef
BikramGyawali <gyawalibikram0@gmail.com>
2026-04-22 19:34:55 +0545

    Fixed: Cors error

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-	origin: "https://hamrojob-backend.onrender.com/",
+	origin: "https://hamrojob.onrender.com",
 	credentials: true
 }));
 

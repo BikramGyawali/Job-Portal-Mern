6573608cb4c65c81591fae28892caaa9a81789d2
BikramGyawali <gyawalibikram0@gmail.com>
2026-06-07 10:36:21 +0545

    running project

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,15 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { resolve } from "path";
-import fs from "fs";
 
 export default defineConfig({
 	plugins: [
 		react(),
 		tailwindcss(),
-		
 	],
+	server: {
+		port: 5173,
+	},
 	build: {
 		outDir: "dist",
 	},
